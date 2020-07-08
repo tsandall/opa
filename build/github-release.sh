edd6d19b409c09c77b7fbed1e64dc1396655304f
Patrick East <east.patrick@gmail.com>
2020-07-08 10:28:32 -0700

    Auto-generate release notes from CHANGELOG
    Signed-off-by: Patrick East <east.patrick@gmail.com>

--- build/github-release.sh
+++ build/github-release.sh
@@ -31,15 +31,24 @@ done
 # Collect a list of opa binaries (expect binaries in the form: opa_<platform>_<arch>[extension])
 ASSETS=()
 for asset in "${ASSET_DIR}"/opa_*_*; do
-  ASSETS+=("-a" "$asset")
+    ASSETS+=("-a" "$asset")
 done
 
+# Gather the release notes from the CHANGELOG for the latest version
+RELEASE_NOTES="release-notes.md"
+
+# The hub CLI expects the first line to be the title
+echo -e "${TAG_NAME}\n" > RELEASE_NOTES
+
+# Fill in the description
+./build/latest-release-notes.sh --output="${RELEASE_NOTES}"
+
+# Update or create a release on github
 if hub release show "${TAG_NAME}" > /dev/null; then
-  # Occurs when the tag is created via GitHub UI w/ a release
-  # Use -m "" to preserve the existing text.
-  hub release edit "${ASSETS[@]}" -m "" "${TAG_NAME}"
+    # Occurs when the tag is created via GitHub UI w/ a release
+    # Use -m "" to preserve the existing text.
+    hub release edit "${ASSETS[@]}" -m "" "${TAG_NAME}"
 else
-  # Create a draft release
-  # TODO: Auto-populate the text from CHANGELOG
-  hub release create "${ASSETS[@]}" -m "${TAG_NAME}" --draft "${TAG_NAME}"
+    # Create a draft release
+    hub release create "${ASSETS[@]}" -F ${RELEASE_NOTES} --draft "${TAG_NAME}"
 fi
